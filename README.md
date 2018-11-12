e28569d5ab61b7eb3fc9ab1652a7b3720c6f070e
brandonjyee <yee.brandon.j@gmail.com>
2018-11-12 17:21:23 -0600

    initial project setup

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Fullstack mini stackathon
 
 https://docs.google.com/document/d/1ev-y_9pOmx4faScFDqtU4NCWDQWhiGIEJzkvwbBAlqY/edit?usp=sharing
+
+First use npm to install http-server globally:
+npm install -g http-server
+
+Then run http-server from this directory. It'll automatically notice the index.html file in the /public folder and serve static files from there.
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+
+    entry: './src/index.js',
+
+    // devServer: {
+    //   contentBase: './public',
+    //   publicPath: '/public'
+    // },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+          {
+            test: [ /\.vert$/, /\.frag$/ ],
+            use: 'raw-loader'
+          }
+        ]
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'CANVAS_RENDERER': JSON.stringify(true),
+            'WEBGL_RENDERER': JSON.stringify(true)
+        })
+    ]
+
+};
