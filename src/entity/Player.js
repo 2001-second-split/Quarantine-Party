ba21dba23275a05ff9524969c18a73d92a176194
brandonjyee <yee.brandon.j@gmail.com>
2018-12-04 15:26:16 -0600

    removed preload() from Player

--- src/entity/Player.js
+++ src/entity/Player.js
@@ -1,15 +1,10 @@
 import 'phaser';
 
 export default class Player extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene, x, y) {
-    super(scene, x, y, 'josh');
+  constructor(scene, x, y, spriteKey) {
+    super(scene, x, y, spriteKey);
 
     // << INITIALIZE PLAYER ATTRIBUTES HERE >>
-  }
-
-  // Load our player spritesheet into game
-  preload() {
-    // << INSERT CODE HERE >>
   }
 
   // Check which controller button is being pushed and execute movement & animation
