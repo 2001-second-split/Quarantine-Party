7d4cfabba6683adc73c312c56193ef18b771a8b7
Tiffany Ma <yunshin.ma@gmail.com>
2020-03-23 22:16:59 -0400

    fix: changed console messages

--- src/server.js
+++ src/server.js
@@ -13,21 +13,13 @@ app.use(express.static(__dirname + '/public'));
 io.on('connection', function (socket) {
   console.log('server: a user connected');
 
-    // create a new player and add it to our players object
+  // create a new player and add it to our players object
   players[socket.id] = {
-    // rotation: 0,
-    // x: Math.floor(Math.random() * 700) + 50,
-    // y: Math.floor(Math.random() * 500) + 50,
     playerId: socket.id,
-    // team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
   };
+
   // send the players object to the new player
   socket.emit('currentPlayers', players);
-
-  // send the star object to the new player
-  // socket.emit('starLocation', star);
-  // send the current scores
-  // socket.emit('scoreUpdate', scores);
 
   // update all other players of the new player
   socket.broadcast.emit('newPlayer', players[socket.id]);
@@ -38,31 +30,21 @@ io.on('connection', function (socket) {
 
     players[socket.id].x = movementData.x;
     players[socket.id].y = movementData.y;
-    players[socket.id].rotation = movementData.rotation;
+
     // emit a message to all players about the player that moved
     socket.broadcast.emit('playerMoved', players[socket.id]);
   });
 
+  // testing key press
   socket.on('testKey', function() {
     console.log('test key pressed');
   })
 
-  // socket.on('starCollected', function () {
-  //   if (players[socket.id].team === 'red') {
-  //     scores.red += 10;
-  //   } else {
-  //     scores.blue += 10;
-  //   }
-  //   star.x = Math.floor(Math.random() * 700) + 50;
-  //   star.y = Math.floor(Math.random() * 500) + 50;
-  //   io.emit('starLocation', star);
-  //   io.emit('scoreUpdate', scores);
-  // });
-
+  // disconnecting
   socket.on('disconnect', function () {
-    console.log('user disconnected');
+    console.log('server - user disconnected');
 
-      // remove this player from our players object
+    // remove this player from our players object
     delete players[socket.id];
     // emit a message to all players to remove this player
     io.emit('disconnect', socket.id);
@@ -70,5 +52,5 @@ io.on('connection', function (socket) {
 });
 
 server.listen(3000, function () {
-  console.log(`Listening on ${server.address().port}`);
+  console.log(`SERVER Listening on ${server.address().port}`);
 });
