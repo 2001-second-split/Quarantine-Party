466fdf0a5a483d6f8409e3926bd0485aa1489f7d
Tiffany Ma <yunshin.ma@gmail.com>
2020-03-26 20:35:22 -0400

    refactor: heroku deploy changes

--- src/server.js
+++ src/server.js
@@ -69,6 +69,7 @@ io.on('connection', (socket)  => {
 
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log(`SERVER Listening on ${server.address().port}`);
 });
