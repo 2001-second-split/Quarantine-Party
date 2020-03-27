5d6026372d6259b44e7ea2da3bcbfc7c603a09a7
Patty Arunyavikul <parunyavikul@gmail.com>
2020-03-27 12:37:16 -0400

    Refactored adding sprites from  WaitFG to Wait Scene

--- src/scenes/WaitFg.js
+++ src/scenes/WaitFg.js
@@ -4,39 +4,12 @@ import { socket } from "../index";
 export default class WaitFg extends Phaser.Scene {
   constructor() {
     super("WaitFg");
-
   }
-
-
+  init(data){
+    console.log(data)
+    this.selectedSprite =data.selectedSprite
+  }
   preload() {
-    // << LOAD SPRITES HERE >>
-
-    this.load.spritesheet("ayse", "assets/spriteSheets/ayse-sheet.png", {
-      frameWidth: 300,
-      frameHeight: 300,
-      endFrame: 8
-    });
-    this.load.spritesheet(
-      "stephanie",
-      "assets/spriteSheets/stephanie-sheet.png",
-      {
-        frameWidth: 300,
-        frameHeight: 300,
-        endFrame: 8
-      }
-    );
-
-    this.load.spritesheet("tiffany", "assets/spriteSheets/tiffany-sheet.png", {
-      frameWidth: 300,
-      frameHeight: 300,
-      endFrame: 8
-    });
-    this.load.spritesheet("patty", "assets/spriteSheets/patty-sheet.png", {
-      frameWidth: 300,
-      frameHeight: 300,
-      endFrame: 8
-    });
-
     this.load.image("platform", "assets/sprites/platform.png");
     // << LOAD SOUNDS HERE >>
     this.load.audio("jump", "assets/audio/jump.wav");
@@ -69,107 +42,91 @@ export default class WaitFg extends Phaser.Scene {
     });
   }
 
-
   create() {
-    let spriteOptions = [];
-
-    spriteOptions.push(this.add.sprite(100, 400, "ayse").setScale(0.8));
-    spriteOptions.push(this.add.sprite(270, 400, "stephanie").setScale(0.8));
-    spriteOptions.push(this.add.sprite(450, 400, "tiffany").setScale(0.8));
-    spriteOptions.push(this.add.sprite(650, 400, "patty").setScale(0.8));
-
-    let totalOptions = spriteOptions.length;
-    let selectedSprite = 0;
-    spriteOptions.forEach(function(sprite) {
-      sprite
-        .setInteractive({ useHandCursor: true })
-        .on("pointerdown", () => {console.log(sprite)});
-    });
-
-
-
-    // Create game entities
-    // << START CREATE GAME ENTITIES HERE >>
-
-    this.createAnimations();
-    this.cursors = this.input.keyboard.createCursorKeys();
-    //create ground
-    this.ground = this.physics.add.staticGroup();
-    this.ground
-      .create(400, 600, "platform")
-      .setScale(2)
-      .refreshBody();
-    // Create sounds
-    this.jumpSound = this.sound.add("jump");
-    // Create collisions for all entities
-    // this.physics.add.collider(this.player, this.ground)
-    // this.physics.add.collider(this.enemy, this.ground)
-    // this.physics.add.collider(this.player, this.enemy)
-    // << END CREATE GAME ENTITIES HERE >>
-    //  << SOCKET THINGS!!! >>
-    this.socket = socket;
-    //AYSE EDIT
-    //this.otherPlayers = this.physics.add.group();
-    this.otherPlayers = [];
-    //console.log(“in waitFG”)
-    // ask the server who current players are
-    this.socket.emit("currentPlayers");
-    //get currentPlayers in room and add self and other players
-    this.socket.on("currentPlayers", (players, room) => {
-      //Find all the players in the same room
-      const playersInRoom = {};
-      Object.keys(players).forEach(id => {
-        if (players[id].roomId === room) {
-          playersInRoom[id] = players[id];
-        }
-      });
-      // console.log(‘Players in room’, playersInRoom)
-      // console.log(‘ROOM’, room)
-      // console.log(‘CURRENT PLAYERS: ’, players)
-      // console.log(‘CURRENT PLAYERS IN ROOM: ’, playersInRoom)
-      // console.log(‘players in room empty until we subscribe’)
-      Object.keys(playersInRoom).forEach(id => {
-        if (players[id].playerId === this.socket.id) {
-          this.addPlayer(players[id]);
-        } else {
-          this.addOtherPlayers(players[id]);
-        }
-      });
-    });
-    //add new players as other players
-    this.socket.on("newPlayer", playerInfo => {
-      console.log("NEW PLAYER HAS JOINED");
-      this.addOtherPlayers(playerInfo);
-    });
-    /*
+    {
+      // Create game entities
+      // << START CREATE GAME ENTITIES HERE >>
+      console.log(this.selectedSprite)
+      this.createAnimations();
+      this.cursors = this.input.keyboard.createCursorKeys();
+      //create ground
+      // this.ground = this.physics.add.staticGroup();
+      // this.ground
+      //   .create(400, 600, "platform")
+      //   .setScale(2)
+      //   .refreshBody();
+      // Create sounds
+      this.jumpSound = this.sound.add("jump");
+      // Create collisions for all entities
+      // this.physics.add.collider(this.player, this.ground)
+      // this.physics.add.collider(this.enemy, this.ground)
+      // this.physics.add.collider(this.player, this.enemy)
+      // << END CREATE GAME ENTITIES HERE >>
+      //  << SOCKET THINGS!!! >>
+      this.socket = socket;
+      //AYSE EDIT
+      //this.otherPlayers = this.physics.add.group();
+      this.otherPlayers = [];
+      //console.log(“in waitFG”)
+      // ask the server who current players are
+      this.socket.emit("currentPlayers");
+      //get currentPlayers in room and add self and other players
+      // this.socket.on("currentPlayers", (players, room) => {
+      //   //Find all the players in the same room
+      //   const playersInRoom = {};
+      //   Object.keys(players).forEach(id => {
+      //     if (players[id].roomId === room) {
+      //       playersInRoom[id] = players[id];
+      //     }
+      //   });
+      //   // console.log(‘Players in room’, playersInRoom)
+      //   // console.log(‘ROOM’, room)
+      //   // console.log(‘CURRENT PLAYERS: ’, players)
+      //   // console.log(‘CURRENT PLAYERS IN ROOM: ’, playersInRoom)
+      //   // console.log(‘players in room empty until we subscribe’)
+      //   Object.keys(playersInRoom).forEach(id => {
+      //     if (players[id].playerId === this.socket.id) {
+      //       this.addPlayer(players[id],selectedSprite);
+      //     } else {
+      //       this.addOtherPlayers(players[id]);
+      //     }
+      //   });
+      // });
+      // //add new players as other players
+      // this.socket.on("newPlayer", playerInfo => {
+      //   console.log("NEW PLAYER HAS JOINED");
+      //   this.addOtherPlayers(playerInfo);
+      // });
+      /*
     we want to have subscribe after the listeners are
     created (above) but now we have to figure out how to
     pass it roomId...
     commented out until we find out how
     */
-    // this.socket.emit(‘subscribe’, roomId.value)
+      // this.socket.emit(‘subscribe’, roomId.value)
+    }
   }
   // update(time, delta) {
   //   // << DO UPDATE LOGIC HERE >>
   //   // this.player.update(this.cursors, this.jumpSound); // Add a parameter for the jumpSound
   // }
   // SOCKET RELATED FUNCTIONS
-
-  addPlayer(playerInfo) {
+  addPlayer(playerInfo, selectedSprite) {
+    console.log("Add player", selectedSprite);
     this.player = new Player(
       this,
       playerInfo.x,
       playerInfo.y,
-      "stephanie"
+      selectedSprite
     ).setScale(0.5);
-    this.player.setCollideWorldBounds(true);
-    this.physics.add(this.ground, this.player);
+    // this.player.setCollideWorldBounds(true);
+    // this.physics.add(this.ground, this.player);
   }
   addOtherPlayers(playerInfo) {
     const otherPlayer = new Player(this, playerInfo.x, playerInfo.y, "ayse");
     otherPlayer.setCollideWorldBounds(true);
-    this.physics.add(this.ground, otherPlayer);
-    this.otherPlayers.push(otherPlayer);
+    // this.physics.add(this.ground, otherPlayer);
+    // this.otherPlayers.push(otherPlayer);
     // const otherPlayer = this.add.sprite(playerInfo.x, playerInfo.y, ‘josh’).setOrigin(0.5, 0.5).setScale(0.5);
     // otherPlayer.playerId = playerInfo.playerId;
     //this.otherPlayers.add(otherPlayer)
--- src/scenes/WaitScene.js
+++ src/scenes/WaitScene.js
@@ -1,16 +1,70 @@
-import 'phaser'
+import "phaser";
 
 export default class WaitScene extends Phaser.Scene {
   constructor() {
-    super('WaitScene');
+    super("WaitScene");
+  }
+
+  preload() {
+    this.load.spritesheet("ayse", "assets/spriteSheets/ayse-sheet.png", {
+      frameWidth: 300,
+      frameHeight: 300,
+      endFrame: 8
+    });
+    this.load.spritesheet(
+      "stephanie",
+      "assets/spriteSheets/stephanie-sheet.png",
+      {
+        frameWidth: 300,
+        frameHeight: 300,
+        endFrame: 8
+      }
+    );
+
+    this.load.spritesheet("tiffany", "assets/spriteSheets/tiffany-sheet.png", {
+      frameWidth: 300,
+      frameHeight: 300,
+      endFrame: 8
+    });
+    this.load.spritesheet("patty", "assets/spriteSheets/patty-sheet.png", {
+      frameWidth: 300,
+      frameHeight: 300,
+      endFrame: 8
+    });
   }
 
   create() {
     // << LOAD BACKGROUND AND FOREGROUND SCENES IN PARALLEL HERE >>
-    this.scene.launch('WaitBg');
-    this.scene.launch('WaitFg');
+    let spriteOptions = [];
+    const ayseSprite = this.add.sprite(100, 400, "ayse").setScale(0.8);
+    const stephanieSprite = this.add
+      .sprite(270, 400, "stephanie")
+      .setScale(0.8);
+    const tiffanySprite = this.add.sprite(450, 400, "tiffany").setScale(0.8);
+    const pattySprite = this.add.sprite(650, 400, "patty").setScale(0.8);
+    ayseSprite.name = "ayse";
+    stephanieSprite.name = "stephanie";
+    tiffanySprite.name - "tiffany";
+    pattySprite.name = "patty";
+    spriteOptions.push(pattySprite);
+    spriteOptions.push(tiffanySprite);
+    spriteOptions.push(stephanieSprite);
+    spriteOptions.push(ayseSprite);
 
+    let selectedSprite = "";
+
+    spriteOptions.forEach(function(sprite) {
+      sprite.setInteractive({ useHandCursor: true }).on("pointerdown", () => {
+        selectedSprite = sprite.name;
+      });
+    });
+
+
+    this.scene.launch("WaitBg");
+
+    if(selectedSprite !== ''){
+
+    this.scene.launch("WaitFg", {selectedSprite: selectedSprite});
+    }
   }
-
 }
-
