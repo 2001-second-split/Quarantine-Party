ef26743a0aae5c8815d548ceae07d8187ab4be7c
Ayse Erduran <ae2456@columbia.edu>
2020-03-28 17:32:06 -0400

    character moves on the board according to the dice the user throws

--- src/scenes/BoardDice.js
+++ src/scenes/BoardDice.js
@@ -1,5 +1,5 @@
 import Dice from '../entity/Dice';
-
+import {socket} from '../index'
 export default class BoardDice extends Phaser.Scene {
   constructor() {
     super('BoardDice');
@@ -20,14 +20,44 @@ export default class BoardDice extends Phaser.Scene {
       frameRate: 10,
       repeat: -1,
     });
+    // this.anims.create({
+    //   key: 'random',
+    //   frames: [{ key: 'dice', frame: Math.floor(Math.random() * 6) }],
+    //   frameRate: 10,
+    // });
+    // this.anims.create({
+    //   key: 'reset',
+    //   frames: [{ key: 'dice', frame: 1 }],
+    //   frameRate: 10,
+    // });
     this.anims.create({
-      key: 'random',
-      frames: [{ key: 'dice', frame: Math.floor(Math.random() * 6) }],
+      key: '1',
+      frames: [{ key: 'dice', frame: 0 }],
       frameRate: 10,
     });
     this.anims.create({
-      key: 'reset',
+      key: '2',
       frames: [{ key: 'dice', frame: 1 }],
+      frameRate: 10,
+    });
+    this.anims.create({
+      key: '3',
+      frames: [{ key: 'dice', frame: 2 }],
+      frameRate: 10,
+    });
+    this.anims.create({
+      key: '4',
+      frames: [{ key: 'dice', frame: 3 }],
+      frameRate: 10,
+    });
+    this.anims.create({
+      key: '5',
+      frames: [{ key: 'dice', frame: 4 }],
+      frameRate: 10,
+    });
+    this.anims.create({
+      key: '6',
+      frames: [{ key: 'dice', frame: 5 }],
       frameRate: 10,
     });
   }
@@ -37,21 +67,23 @@ export default class BoardDice extends Phaser.Scene {
 
     this.diceRollAnimations();
     this.dice.setInteractive();
-
-    this.dice.on('pointerup', function () {
-      // console.log('dice clicked');
-      this.dice.rollDice()
-    }, this);
-
     // Create sounds
     // placeholder for dice roll sound
     // this.diceSound = this.sound.add('dice');
+    this.dice.on('pointerup', function () {
+      // console.log('dice clicked');
+      this.dice.rollDice()
+      socket.emit('diceRoll', this.dice.rolledNum)
+      //console.log(this.dice.rolledNum)
+    }, this);
   }
+
 
   // time: total time elapsed (ms)
   // delta: time elapsed (ms) since last update() call. 16.666 ms @ 60fps
   update(time, delta) {
     // this.dice.update(this.cursors, this.diceSound); // Add a parameter for the diceSound
     // this.dice.update()
+
   }
 }
