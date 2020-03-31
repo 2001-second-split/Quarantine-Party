1d1d1fb2f7720f6367ba878f415cb21c6adc6dca
Tiffany Ma <yunshin.ma@gmail.com>
2020-03-31 17:11:11 -0400

    fix

--- src/scenes/BoardBg.js
+++ src/scenes/BoardBg.js
@@ -88,7 +88,7 @@ export default class BoardBg extends Phaser.Scene {
     //listen for minigames
     socket.on('minigameStarted', () => {
       //make the current scene sleep + starts minigame
-      this.scene.add('minigameTPScene')
+      this.scene.add('minigameTPScene', minigameTPScene)
       this.scene.switch('minigameTPScene', {queue: this.queue, player: this.player, otherPlayers: this.otherPlayers})
     })
   }
